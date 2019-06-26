5faa5222298e921b6777dadd849b11910668e50b
vamsi <vamsi@DESKTOP-2QOB21C>
2019-06-26 11:48:38 +0530

    commit

---
+++ Day3/src/vamsi/tut1.java
@@ -0,0 +1,20 @@
+package vamsi;
+
+public class tut1 {
+public static void main(String[] args) {
+	int i,j,n=4;
+	for(i=1;i<=n;i++)
+		
+	{
+		for(j=i;j<=(i*2)+1;j+=3)
+		{
+			System.out.print(j);
+		}
+		for(i=3;i<=n;i    ++)
+		{
+			System.out.print((i+j)-1);
+	}
+		System.out.println();
+	}
+}
+}
