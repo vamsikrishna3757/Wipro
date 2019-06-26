b027caee0174dd08c7b620091cd14a0d00e7545f
vamsi <vamsi@DESKTOP-2QOB21C>
2019-06-26 10:52:03 +0530

    commit

---
+++ Day3/src/vamsi/upperlower.java
@@ -0,0 +1,22 @@
+package vamsi;
+
+public class upperlower {
+	public static void main(String[] args) {
+		char a='L';
+		if(a>='A'&&a<='Z')
+		{
+			System.out.println((char)(a+32));
+		}
+		if(a>='a'&&a<='z')
+		{
+			System.out.println((char)(a-32));
+		}
+		
+		
+		
+		
+			
+			
+		}
+
+}
