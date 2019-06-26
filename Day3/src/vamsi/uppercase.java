4ceebecabffe4af324dbf1589a92037332bbfdfa
vamsi <vamsi@DESKTOP-2QOB21C>
2019-06-26 10:45:54 +0530

    commit

---
+++ Day3/src/vamsi/uppercase.java
@@ -0,0 +1,23 @@
+package vamsi;
+
+
+public class uppercase {
+	public static void main(String[] args) {
+	char a='L';
+	if(a>='A'&&a<='Z')
+	{
+		System.out.println((char)(a+32));
+	}
+	if(a>='a'&&a<='z')
+	{
+		System.out.println((char)(a-32));
+	}
+	
+	
+	
+	
+		
+		
+	}
+
+}
