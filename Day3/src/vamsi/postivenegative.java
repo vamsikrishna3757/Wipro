ee48fd21d36a061338df4c6836ec585c07fd1fa5
vamsi <vamsi@DESKTOP-2QOB21C>
2019-06-26 11:40:28 +0530

    commit

--- Day3/src/vamsi/postivenegative.java
+++ Day3/src/vamsi/postivenegative.java
@@ -1,5 +1,28 @@
 package vamsi;
 
+import java.util.Scanner;
+
 public class postivenegative {
+	public static void main(String[] args)
+	{
+		int i,j;
+		System.out.println("enter the number: ");
+		Scanner sc= new Scanner(System.in);
+		 i=sc.nextInt();
+		 if(i>0)
+		 {
+			 System.out.println("The number is positive");
+		 }
+		 else if(i<0)
+		 {
+			 System.out.println("the number is negative");
+			 
+		 }
+		 else {
+			 System.out.println("number is equal");
+		 }
+		
+		
+	}
 
 }
