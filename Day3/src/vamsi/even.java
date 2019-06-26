02a0694f17b02e7be461532a4074632ee20d2d92
vamsi <vamsi@DESKTOP-2QOB21C>
2019-06-26 12:03:36 +0530

    commit

---
+++ Day3/src/vamsi/even.java
@@ -0,0 +1,17 @@
+package vamsi;
+
+public class even {
+	public static void main(String[] args) {
+		int i=6;
+		System.out.println("enter the number: ");
+		if (i%2==0)
+		{
+			System.out.println("number is even");
+		}
+		else {
+			System.out.println("number is odd");
+		}
+	}
+	
+
+}
